081667add3a8d2ee7857416cdca08b431b95c486
MANISH MAHESH TALREJA <MANISHTALREJA189@GMAIL.COM>
2026-01-02 22:38:55 +0530

    DONE

---
+++ 961.java
@@ -0,0 +1,34 @@
+import java.util.Hashtable;
+import java.util.Map;
+
+
+class Solution
+{
+    public int repeatedNTimes(int[] nums)
+    {
+        Hashtable<Integer, Integer> repeated = new Hashtable<>();
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(repeated.containsKey(nums[i]))
+            {
+                Integer value = repeated.get(nums[i]);
+                repeated.put(nums[i], value + 1);
+            }
+            else
+            {
+                repeated.put(nums[i], 1);
+            }
+        }
+        int max = 0;
+        int output = 0;
+        for(Map.Entry<Integer, Integer> value: repeated.entrySet())
+        {
+            if(value.getValue() > max)
+            {
+                max = value.getValue();
+                output = value.getKey();
+            }
+        }
+        return output;
+    }
+}
