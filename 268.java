f3863b6c097c2b0f1dbb4c3ca0c80dd1fd687919
MANISH MAHESH TALREJA <MANISHTALREJA189@GMAIL.COM>
2026-01-10 00:17:22 +0530

    DONE

---
+++ 268.java
@@ -0,0 +1,15 @@
+class Solution
+{
+    public int missingNumber(int[] nums)
+    {
+        Arrays.sort(nums);
+        for(int i = 0; i < nums[nums.length - 1]; i++)
+        {
+            if(i != nums[i])
+            {
+                return i;
+            }
+        }
+        return nums[nums.length - 1] + 1;
+    }
+}
